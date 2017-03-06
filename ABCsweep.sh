d39fff389c97147f472f422366273efc7ffb1218
popgenomics <camille.roux.1@unil.ch>
2017-03-06 17:02:28 +0000

    trying to resolve swap issues

--- ABCsweep.sh
+++ ABCsweep.sh
@@ -2,7 +2,8 @@
 #./ABCsweep.sh nInd nRep
 nInd=$1 # number of simulated individuals
 nRep=$2 # number of times a combination of parameters is replicated
-priorfile=$3 # file containing the combination of parameters
+length=$3 # number of nucleotides of the surveyed region 
+priorfile=$4 # file containing the combination of parameters
 
 # prepare replicates
 nSimul=0
@@ -16,8 +17,8 @@ done <$priorfile
 nCombParam=$(wc -l $priorfile | awk '{print $1}')
 
 mkfifo fifo
-./msmscalc.py fifo $nInd $nRep $nCombParam &
-cat ${priorfile}_tmp | msms $nInd $nSimul -s tbs -r 80 10000 -SAA tbs -SaA 100 -SF 0 -Sp 0.5 -N 100000  >fifo
+./msmscalc.py fifo $nInd $nRep $nCombParam $length&
+cat ${priorfile}_tmp | msms $nInd $nSimul -s tbs -r 80 $length -SAA tbs -SaA 100 -SF 0 -Sp 0.5 -N 100000  >fifo
 rm fifo
 rm ${priorfile}_tmp
  
