9bd6f63245bea8214b9513b884abdcab5fc093d2
popgenomics <camille.roux.1@unil.ch>
2017-03-07 11:55:32 +0000

    to infer three parameters: SAA, time and position

--- ABCsweep.sh
+++ ABCsweep.sh
@@ -17,8 +17,8 @@ done <$priorfile
 nCombParam=$(wc -l $priorfile | awk '{print $1}')
 
 mkfifo fifo
-./msmscalc.py fifo $nInd $nRep $nCombParam $length&
-cat ${priorfile}_tmp | msms $nInd $nSimul -s tbs -r 80 $length -SAA tbs -SaA 100 -SF 0 -Sp 1 -N 100000  >fifo
+./msmscalc.py fifo $nInd $nRep $nCombParam $length &
+cat ${priorfile}_tmp | msms $nInd $nSimul -s 500 -r 100 $length -SAA tbs -SaA 100 -SF tbs -Sp tbs -N 100000  >fifo
 rm fifo
 rm ${priorfile}_tmp
  
