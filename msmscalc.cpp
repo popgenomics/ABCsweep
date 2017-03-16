759e73833a808b355174ac448ff3cb7c57b7b0e5
popgenomics <roux@tauraduboudin@gmail.com>
2017-03-16 12:33:08 +0000

    C++ version of msmscalc.py

--- msmscalc.cpp
+++ msmscalc.cpp
@@ -9,34 +9,48 @@ class Bins{
 	// object of class Bins contains two vectors 'min' and 'max'
 	// corresponding to the coordinates of all explored windows over a sequence
 	// with a given width and step
+	//methods:
 	public:
-	size_t nElements;
-	std::vector<float> min;
-	std::vector<float> max;
+	void window(float width, float step);
+	void printBins();
+
+	// attributes
+	private:
+	size_t m_nElements;
+	std::vector<float> m_min;
+	std::vector<float> m_max;
 };
 
 
 class Alignment{
+	// methods:
 	public:
-	int nIndiv;
-	int nReplicate;
-	std::vector<std::string> dataset; // dataset[over replicates][over individuals] -> one sequence per individual
-	
+	Alignment();
+	void ajouteReplicat(); 
+
+	// attributes:
+	private:
+	unsigned m_nReplicate;
+	std::vector<unsigned> m_nIndiv;
+	std::vector<unsigned> m_nSNP;
+	std::vector< std::vector<std::string> > m_dataset; // dataset[over replicates][over individuals] -> one sequence per individual
+	std::vector< std::vector<float> > m_position; // dataset[over replicates][over SNPs] -> one position per SNP
 };
 
 
-Bins window(float width=0.1, float step=0.05);
+//Bins window(float width=0.1, float step=0.05);
 
 // arg1 = name of the msms outputfile
 int main(int argc, char* argv[]){
 	const std::string msmsFile(argv[1]); // mmsFile contains the name of msms output file to read
-	const int nIndiv = atoi(argv[2]); // number of individuals in the alignment
-	const int nCombParam = atoi(argv[3]); // number of combination of parameters
-	const int nReplicate = atoi(argv[4]); // number of replicates per combination of parameters: n sim tot = nCombParam * nReplicate
+	const unsigned nIndiv = atoi(argv[2]); // number of individuals in the alignment
+	const unsigned nCombParam = atoi(argv[3]); // number of combination of parameters
+	const unsigned nReplicate = atoi(argv[4]); // number of replicates per combination of parameters: n sim tot = nCombParam * nReplicate
 
 	// define the boundaries of the surveyed windows over a sequence
 	Bins bins;
-	bins = window();
+	bins.window(0.1, 0.05);
+	bins.printBins();
 
 	// read the msms outputfile
 	std::ifstream fifo(msmsFile.c_str());
@@ -48,7 +62,7 @@ int main(int argc, char* argv[]){
 		unsigned nSNPs(0); // number of SNPs for a given dataset
 		int test(-1); // =-1 at the 'segsites' line. setted to +1 at the 'positions' line. haplotypes are only readen if test==1
 		unsigned cntIndiv(0); // count the number of haplotypes after the 'positions' line for a given dataset
-		std::vector<std::string> alignement;
+		std::vector<std::string> alignement; // contains nIndiv haplotypes
 
 		while(std::getline(fifo, ligne)){ // read the msmsFile
 
@@ -90,7 +104,8 @@ int main(int argc, char* argv[]){
 			} // end of: "if the line contains the string: 'positions'
 
 
-			if(test == 1 && ligne!="" && ligne[0]!='/' && ligne[0]!='s' && ligne[0]!='p'){
+			if(test == 1 && ligne!="" && ligne[0]!='/' && ligne[0]!='s' && ligne[0]!='p'){ /* If the line is not
+				empty, and doesn't start by '/', nor 'segsites', nor 'positions'*/
 				if(cntIndiv < (nIndiv+1)){
 					cntIndiv++;
 					alignement.push_back(ligne);
@@ -99,10 +114,10 @@ int main(int argc, char* argv[]){
 				if(cntIndiv == nIndiv){
 					nDataset++;
 					for(i=0; i<nIndiv; i++){
-						std::cout << i << ": " << alignement[i] << std::endl;
+					//	std::cout << i << ": " << alignement[i] << std::endl;
 					}
 					std::cout << "end of treatment of dataset " << nDataset <<
-					" from replicate " << nDataset%nReplicate << std::endl << std::endl;
+					" from replicate " << nDataset%nReplicate << std::endl ;
 				}
 			}
 			
@@ -112,43 +127,54 @@ int main(int argc, char* argv[]){
 		exit(0);
 	}	
 	
-	// test
-	/* // test for the bins
-	Bins test;
-	test = window();
-	size_t i = 0;
-	for(i=0; i<test.nElements; i++){
-		std::cout << "min = " << test.min[i] << "\tmax = " << test.max[i] << std::endl;
-	}*/
 	return(0);
 }
 
 
-Bins window(float width, float step){
+// methods for class 'Bins'
+void Bins::window(float width, float step){
 	/* Function that return an object of class Bins
 	with the positions of the 'min' and 'max' boundaries
 	of windows over a sequence */
-	Bins res;
-	size_t nElements = 1;
-	float minB = 0;
+	m_nElements = 1;
+	float minB = 0.0;
 	float maxB = width;
 
-	res.min.push_back(minB);
-	res.max.push_back(maxB);
+	m_min.push_back(minB);
+	m_max.push_back(maxB);
 
 	while((minB + width) <= 1 && maxB < 1){
-		nElements++;
+		m_nElements++;
 		minB += step;
 		maxB += step;
 		if(maxB > 1){
 			maxB = 1;
 		}
-	res.min.push_back(minB);
-	res.max.push_back(maxB);
+	m_min.push_back(minB);
+	m_max.push_back(maxB);
 	}
-	
-	res.nElements = nElements;
+}
 
-	return(res);
+void Bins::printBins(){
+	size_t i(0);
+	for(i=0; i<m_nElements; i++){
+		std::cout << "bin #" << i << "\tmin = " << m_min[i] << "\tmax = " << m_max[i] << std::endl;
+	}
+}
+
+
+// methods for class 'Alignment'
+// constructeur
+Alignment::Alignment() : m_nReplicate(0)
+{
+}
+
+void Alignment::ajouteReplicat(){ 
+	std::vector <std::string> tmp_seq;
+	std::vector <float> tmp_pos;
+	m_nIndiv.push_back(0); // vector of nReplicate values of nIndiv
+	m_nSNP.push_back(0); // vector of nReplicate values of nSNPs
+	m_dataset.push_back(tmp_seq); // rReplicate vectors, each containing nIndiv sequences
+	m_position.push_back(tmp_pos); // nReplicate vectors, each containing nSNP positions
 }
 
